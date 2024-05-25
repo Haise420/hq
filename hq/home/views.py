57a81eb044ba5a519bb5f54f45026c121a774589
Sone04 <srdicnebojsa78@gmail.com>
2024-05-26 00:51:50 +0200

    1.3 update ide gas

--- hq/home/views.py
+++ hq/home/views.py
@@ -7,8 +7,10 @@ from django.conf import settings
 
 @login_required(login_url=settings.LOGIN_URL)
 def home(request):
+
+    korisnik = request.user.clan
     beleske = Beleska.objects.all()
-    context = {'beleske': beleske}
+    context = {'beleske': beleske, 'korisnik':korisnik}
 
     return render(request, 'home/home.html', context)
 
@@ -71,14 +73,17 @@ def profil(request):
 def projekti(request):
     projekti = Projekat.objects.all()
     clanovi = Clan.objects.all()
+    korisnik = request.user.clan
+
+
 
     if request.method == "POST":
         ime = request.POST.get('ime')
         deskripcija = request.POST.get('deskripcija')
         clanovii = request.POST.getlist('clanovi')
-        slika = request.POST.get('slika')
+        slika = request.FILES['slika']
 
-        novi_projekat = Projekat(ime=ime, deskripcija=deskripcija, slika=slika)
+        novi_projekat = Projekat(ime=ime, deskripcija=deskripcija, slika=slika, aktivnost=korisnik)
         novi_projekat.save()
 
         if clanovii:
@@ -87,53 +92,79 @@ def projekti(request):
 
         return redirect('projekti')
 
-    context = {'projekti': projekti, 'clanovi': clanovi}
+    context = {'projekti': projekti, 'clanovi': clanovi, 'korisnik':korisnik}
     
     return render(request, 'home/projekti.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
 def clanovi(request):
-
+     
+    korisnik = request.user.clan
     clanovi = Clan.objects.all()
-    context = {'clanovi': clanovi}
+    context = {'clanovi': clanovi, 'korisnik':korisnik}
     
     return render(request, 'home/clanovi.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
 def webftp(request):
 
+    korisnik = request.user.clan
 
+    context={'korisnik':korisnik}
 
-    return render(request, 'home/webftp.html')
+    return render(request, 'home/webftp.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
 def masine(request):
-
+    korisnik = request.user.clan
     masine = Masina.objects.all()
-    context = {'masine': masine}
+    context = {'masine': masine, 'korisnik':korisnik}
     
     return render(request, 'home/masine.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
+def pregledaj_masinu(request, masina_id):
+    
+
+    masina = Masina.objects.get(id=masina_id)
+
+    context = {'masina': masina}
+    return render(request, 'home/pregledaj_masinu.html', context)
+
+@login_required(login_url=settings.LOGIN_URL)
+def obrisi_masinu(request, masina_id):
+    masina = Masina.objects.get(id=masina_id)
+
+    masina.delete()
+
+ 
+    return redirect('masine')
+
+@login_required(login_url=settings.LOGIN_URL)
 def copilot(request):
 
+    korisnik = request.user.clan
+    context = {'korisnik':korisnik}
 
 
-    return render(request, 'home/copilot.html')
+    return render(request, 'home/copilot.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
 def planovi(request):
+    
+    korisnik = request.user.clan
+    context = {'korisnik':korisnik}
 
 
-
-    return render(request, 'home/planovi.html')
+    return render(request, 'home/planovi.html', context)
 
 @login_required(login_url=settings.LOGIN_URL)
 def obuka(request):
+    korisnik = request.user.clan 
+    context = {'korisnik':korisnik}
 
 
-
-    return render(request, 'home/obuka.html')
+    return render(request, 'home/obuka.html', context)
 
 def login_view(request):
     context = {}
---
+++ hq/static/js/modal.js
@@ -0,0 +1,9 @@
+
+function otvoriModal(){
+    var modal = document.getElementById("modal");
+    modal.style.display = 'block';
+}
+function zatvoriModal(){
+    var modal = document.getElementById("modal");
+    modal.style.display = 'none';
+}
